901e74beadc980805b85db0c5e5631524232dc85
Douwe van Gijn <kipstad@hotmail.com>
2016-10-31 12:24:11 +0100

    added tessellate.h

---
+++ tessellate.h
@@ -0,0 +1,13 @@
+int tessellate(stbtt__point** out, const stbtt_fontinfo *info, int glyph) {
+    stbtt__vertex* vertices;
+    int num_verts = stbtt_GetGlyphShape(info, glyph, &vertices);
+    
+    float flatness_in_pixels = 0.35f;
+    float scale = scale_x > scale_y ? scale_y : scale_x;
+    int winding_count, *winding_lengths;
+    stbtt__point* windings =
+        stbtt_FlattenCurves(vertices, num_verts, flatness_in_pixels / scale,
+                            &winding_lengths, &winding_count, info->userdata);
+    *out = windings;
+    return winding_count;
+};
